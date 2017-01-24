1259761c6201fcab83c4abe8e06ad32b50916453
Vasil Pachedzhiev <vasil.pachedzhiev@novarto.com>
2017-01-24 21:26:36 +0200

    material ui added

--- webapp/src/index.jsx
+++ webapp/src/index.jsx
@@ -1,4 +1,22 @@
 import React from "react"
 import ReactDom from "react-dom"
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import darkBaseTheme from "material-ui/styles/baseThemes/darkBaseTheme";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import FlatButton from 'material-ui/FlatButton'
+import Paper from 'material-ui/Paper';
+injectTapEventPlugin()
 
-ReactDom.render(<div><h1>React work Vekeee</h1></div>, document.getElementById('brickBreaker'));
+const App = () => (
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <Paper>
+            <FlatButton label="Default" />
+            <FlatButton label="Primary" primary={true} />
+            <FlatButton label="Secondary" secondary={true} />
+            <FlatButton label="Disabled" disabled={true} />
+        </Paper>
+    </MuiThemeProvider>
+)
+
+ReactDom.render(<App/>, document.getElementById('brickBreaker'));
